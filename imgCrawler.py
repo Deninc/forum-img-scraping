9b8b3f526f3c64d3d551b09da1c9595e9bf2559c
Thang Nguyen <deninc@Thangs-MacBook-Air.local>
2016-09-15 23:12:10 +0200

    change to OOP style

--- imgCrawler.py
+++ imgCrawler.py
@@ -3,7 +3,57 @@ import requests, argparse, os, threading
 
 DIR = "./img"
 
-def arguments():
+class VozCrawler():
+    ''' Crawling a topic in vozforums '''
+    def __init__(self, topicID, pages, dest):
+        self.topicID = topicID
+        self.pages = set(pages)
+        self.dest = dest
+
+    def download(self):
+        '''Multi-thread download'''
+        downloadThreads = []
+        for pageID in self.pages:
+            # each thread download one page concurrently
+            downloadThread = threading.Thread(target=self._downloadPage, args=(pageID,))
+            downloadThreads.append(downloadThread)
+            downloadThread.start()
+            
+        # wait for all threads to finish
+        for thread in downloadThreads:
+            thread.join()
+        print("All threads finished")
+
+    def _downloadPage(self, pageID):
+        '''download all user posted images in page
+            form: ../showthread.php?t=topicID&page=pageID
+        '''
+        res = requests.get("https://vozforums.com/showthread.php?t={}&page={}".format(self.topicID, pageID))
+        soup = BeautifulSoup(res.text, "html.parser")
+        imgs = soup.select(".voz-post-message img")
+        srcs = set([img.get("src") for img in imgs])
+        for src in srcs:
+            if src.startswith("http"):
+                self.downloadImage(src, self.dest)
+
+    @staticmethod
+    def downloadImage(url, dest):
+        '''download an image from the url, save to dir'''
+        print("Downloading image from %s" % url)
+        try:
+            res = requests.get(url)
+            res.raise_for_status()
+            imgName = os.path.basename(url) # get the 'abc.jpg' in the url
+            imgDes = os.path.join(dest, imgName) # /abc.jpg for OS X and \\abc.jpg for Windows
+            imgFile = open(imgDes, "wb")
+            for chunk in res.iter_content(100000):
+                imgFile.write(chunk)
+            imgFile.close()
+        except Exception as exc:
+            print("Couldn't download img: %s" % exc)
+
+
+def getArguments():
     ''' command-line options parser
     imgCrawler topicID -p pageNum1 [pageNum2...] -> separate pages
     imgCrawler topicID -r pageNum1 pageNum2 -> pages range
@@ -15,68 +65,17 @@ def arguments():
     group.add_argument("--range", "-r", metavar=("start", "end"), help="range of pages", type=int, nargs=2)
 
     args = parser.parse_args()
-    return args
-
-
-def downloadImage(url):
-    '''download an image from the url, save to DIR'''
-    print("Downloading image from %s" % url)
-    try:
-        res = requests.get(url)
-        res.raise_for_status()
-        imgName = os.path.basename(url) # get the 'abc.jpg' in the url
-        imgDes = os.path.join(DIR, imgName) # /abc.jpg for OS X and \\abc.jpg for Windows
-        imgFile = open(imgDes, "wb") # write binary mode
-        for chunk in res.iter_content(100000):
-            imgFile.write(chunk)
-        imgFile.close()
-        print("Image downloaded")
-    except Exception as exc:
-        print("Couldn't download img: %s" % exc)
-
-
-def downloadPage(topicID, pageID):
-    '''download all images from one page'''
-    res = requests.get("https://vozforums.com/showthread.php?t={}&page={}".format(topicID, pageID))
-    soup = BeautifulSoup(res.text, "html.parser")
-    imgs = soup.select(".voz-post-message img")
-    srcs = set([img.get("src") for img in imgs])
-    for src in srcs:
-        if src.startswith("http"):
-            downloadImage(src)
-
-
-def download(args):
-    '''download all images from all pages'''
-    topicID = args.topicID
     pages = list(set(args.pages)) if args.pages else range(args.range[0], args.range[1]+1)
-    for pageID in pages:
-        downloadPage(topicID, pageID)
 
-
-def downloadMultiThreads(args):
-    '''MULTI THREAD IMPLEMENTATION'''
-    topicID = args.topicID
-    pages = list(set(args.pages)) if args.pages else range(args.range[0], args.range[1]+1)
-    #downloadThreads = [] # list of Thread objects
-    for pageID in pages:
-        # each thread download one page concurrently
-        downloadThread = threading.Thread(target=downloadPage, args=(topicID, pageID))
-    #   downloadThreads.append(downloadThread)
-        downloadThread.start()
-    # wait for all threads to finish before execute codes after this function
-    #for thread in downloadThreads:
-    #   thread.join() # wait until finish
+    return args.topicID, pages
 
 def main():
     if not os.path.exists(DIR):
         os.makedirs(DIR)
-    #import time
-    #start = time.time()
-    #download(parser()) # 12301 -r 540 543 -> 47.776850938797
-    downloadMultiThreads(arguments()) # 12301 -r 540 543 -> 11.147968053817749
-    #elapsed = time.time()
-    #print("Time spent: %s" % (elapsed-start))
+
+    topic, pages = getArguments()
+    c = VozCrawler(topic, pages, DIR)
+    c.download()
 
 if __name__ == "__main__":
     main()
